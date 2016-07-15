122f1d45167530c2ea0187972d9603d8a044aba1
avinash-chopra <avinash.chopra@metacube.com>
2016-07-15 17:22:48 +0530

    Update TrianglePattern.java

--- PF Assignment 2/src/TrianglePattern.java
+++ PF Assignment 2/src/TrianglePattern.java
@@ -48,7 +48,7 @@ public class TrianglePattern {
 		try{
 			inputNumber = Integer.parseInt(br.readLine());
 		}catch(Exception e){
-			System.out.println("Please Enter the Valid Binary Number");
+			System.out.println("Please Enter the Valid Number");
 			 inputNumber = inputMethod();
 		}
 		return inputNumber;
