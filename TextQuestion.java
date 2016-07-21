8c431ec94e2d522222b75eebed4e7b7e43a11165
avinash-chopra <avinash.chopra@metacube.com>
2016-07-21 21:59:12 +0530

    Update TextQuestion.java

--- TextQuestion.java
+++ TextQuestion.java
@@ -1,20 +1,28 @@
-/**
- * 
- */
 package session2;
 
 import java.io.IOException;
-
 /**
- * @author admin
- *
+ * 
+ * @author Avinash Chopra 
+ * @Date 21 july 2016 
+ * This class use to get feedback from user
+ *         
  */
 public class TextQuestion {
 	GetInput getInput = new GetInput();
-	public void textSelectQuestion(int user,String message,String feedback[]) throws IOException {
+	/**
+	 * 
+	 * @param user 				// user inputing the value
+	 * @param message			// what message should appear
+	 * @param feedback			// get the feedback array
+	*  @return				// return string which inputed by user
+	 * @throws IOException		// if user give wrong input then through exception message
+	 */
+	public String textSelectQuestion(int user,String message,String feedback[]) throws IOException {
 		String tempMessage = "hello user " + (user+1) + " Enter the " + message ;
 		String tempInput = getInput.getInput(tempMessage);
 		feedback[user] = tempInput;
+		return tempInput;
 	}
 
 }
