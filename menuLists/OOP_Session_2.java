4dc27ac4b2b327e5b5d00a75b073268e56ff875d
avinash-chopra <avinash.chopra@metacube.com>
2016-07-25 22:41:50 +0530

    menu List on class Based

---
+++ menuLists/OOP_Session_2.java
@@ -0,0 +1,14 @@
+package menuItemClassBased;
+
+public class OOP_Session_2 extends ObjectOrientedDesign{
+	
+public void action(){
+		
+		System.out.println("OOP Session 2");
+		
+		System.out.println("1. Back");
+		
+		System.out.println("2. Exit");
+	}
+
+}
